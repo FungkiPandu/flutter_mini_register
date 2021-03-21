aa8b9c71050bba8e0ea392693dbd394fba793e61
Fungki Pandu F <neopandu@gmail.com>
2021-03-21 22:55:21 +0700

    feat: create custom time picker

---
+++ lib/component/my_time_picker.dart
@@ -0,0 +1,117 @@
+import 'dart:io';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_mini_register/component/my_text.dart';
+import 'package:flutter_mini_register/helper/datetime.dart';
+
+class MyTimePicker extends StatelessWidget {
+  final String? caption;
+  final DateTime? currentDateTime;
+  final void Function(DateTime) onDateTimeChanged;
+
+  const MyTimePicker({
+    Key? key,
+    this.caption,
+    this.currentDateTime,
+    required this.onDateTimeChanged,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    var captionHeader = <Widget>[];
+    if (caption != null) {
+      captionHeader.addAll([
+        MyText(
+          caption!,
+          type: MyTextType.body,
+          color: Colors.white,
+          fontWeight: FontWeight.bold,
+        ),
+        SizedBox(height: 8),
+      ]);
+    }
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        ...captionHeader,
+        _pickerByPlatform(context),
+      ],
+    );
+  }
+
+  Widget _pickerByPlatform(BuildContext context) {
+    if (Platform.isIOS) return _iOSDatePicker(context);
+    return _materialDatePicker(context);
+  }
+
+  Widget _iOSDatePicker(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(12),
+      ),
+      padding: EdgeInsets.only(left: 16, right: 8),
+      child: CupertinoDialogAction(
+        child: Text(currentDateTime != null
+            ? DateTimeHelper(currentDateTime!).formattedDate
+            : "- Chose Date -"),
+        textStyle: MyText.typeStyle(MyTextType.title),
+        onPressed: () {
+          showCupertinoModalPopup(
+            context: context,
+            builder: (context) => Container(
+              height: 220,
+              child: CupertinoDatePicker(
+                mode: CupertinoDatePickerMode.time,
+                onDateTimeChanged: onDateTimeChanged,
+                initialDateTime: currentDateTime,
+                minimumDate: DateTime.now(),
+              ),
+              color: Colors.white,
+            ),
+          );
+        },
+      ),
+    );
+  }
+
+  Widget _materialDatePicker(BuildContext context) {
+    return InkWell(
+      borderRadius: BorderRadius.circular(12),
+      child: Container(
+        width: MediaQuery.of(context).size.width,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(12),
+        ),
+        padding: EdgeInsets.symmetric(vertical: 12, horizontal: 16),
+        child: Row(
+          mainAxisSize: MainAxisSize.max,
+          children: [
+            Expanded(
+              child: MyText(
+                currentDateTime != null
+                    ? DateTimeHelper(currentDateTime!).formattedTime
+                    : "- Chose Date -",
+                type: MyTextType.title,
+              ),
+            ),
+            Icon(Icons.arrow_drop_down, size: 24),
+          ],
+        ),
+      ),
+      onTap: () {
+        var now = DateTime.now();
+        showTimePicker(
+          context: context,
+          initialTime: TimeOfDay.fromDateTime(currentDateTime ?? now),
+        ).then(
+          (TimeOfDay? value) => (value != null)
+              ? onDateTimeChanged(DateTimeHelper.toDateTime(value))
+              : null,
+        );
+      },
+    );
+  }
+}
--- lib/helper/datetime.dart
+++ lib/helper/datetime.dart
@@ -1,9 +1,19 @@
+import 'package:flutter/material.dart';
+
 class DateTimeHelper {
   final DateTime date;
 
   DateTimeHelper(this.date);
 
   String get formattedDate => "$dayName, ${date.day} $monthName ${date.year}";
+
+  String get formattedTime => "${twoDigit(date.hour)}:${twoDigit(date.minute)}";
+
+  String twoDigit(int value) {
+    var digits = value.toString();
+    if (value < 10 && value >= 0) digits = "0$digits";
+    return digits;
+  }
 
   get dayName {
     switch (date.weekday) {
@@ -41,5 +51,20 @@ class DateTimeHelper {
       "Desember",
     ];
     return months[date.month - 1];
+  }
+
+  static toDateTime(TimeOfDay time) {
+    final now = new DateTime.now();
+    return new DateTime(now.year, now.month, now.day, time.hour, time.minute);
+  }
+
+  static bindDateAndTime(DateTime date, TimeOfDay time) {
+    return new DateTime(
+        date.year, date.month, date.day, time.hour, time.minute);
+  }
+
+  static bindDateTime(DateTime date, DateTime time) {
+    return new DateTime(
+        date.year, date.month, date.day, time.hour, time.minute);
   }
 }
