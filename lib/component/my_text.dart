200879bea824a384189bf82f7719b62891e4efa8
Fungki Pandu F <neopandu@gmail.com>
2021-03-21 13:35:22 +0700

    feat: able to change font weight on MyText

--- lib/component/my_text.dart
+++ lib/component/my_text.dart
@@ -6,52 +6,61 @@ class MyText extends StatelessWidget {
   final String text;
   final Color color;
   final MyTextType type;
+  final FontWeight? fontWeight;
 
   const MyText(
     this.text, {
     Key? key,
     this.color = Colors.black,
     this.type = MyTextType.body,
+    this.fontWeight,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Text(
       text,
-      style: typeStyle(type, color: color),
+      style: typeStyle(
+        type,
+        color: color,
+        fontWeight: fontWeight,
+      ),
     );
   }
 
   static TextStyle typeStyle(
     MyTextType type, {
     Color color = Colors.black,
+    FontWeight? fontWeight,
   }) {
     var fontSize = 14.0;
-    var fontWeight = FontWeight.normal;
+    var _fontWeight = FontWeight.normal;
 
     switch (type) {
       case MyTextType.h1:
         fontSize = 42;
-        fontWeight = FontWeight.bold;
+        _fontWeight = FontWeight.bold;
         break;
       case MyTextType.h2:
         fontSize = 28;
-        fontWeight = FontWeight.bold;
+        _fontWeight = FontWeight.bold;
         break;
       case MyTextType.title:
         fontSize = 16;
-        fontWeight = FontWeight.w600;
+        _fontWeight = FontWeight.w600;
         break;
       case MyTextType.body:
-        fontSize = 15;
-        fontWeight = FontWeight.normal;
+        fontSize = 14;
+        _fontWeight = FontWeight.normal;
         break;
     }
+
+    if (fontWeight != null) _fontWeight = fontWeight;
 
     return TextStyle(
       color: color,
       fontSize: fontSize,
-      fontWeight: fontWeight,
+      fontWeight: _fontWeight,
     );
   }
 }
